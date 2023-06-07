532fce2b67765bf38b682fb57ac97bba9fe7cd86
amirda <amirda.ganbold@gmail.com>
2023-06-08 00:08:26 +0800

    Finance module hiij ehelsen. Expense, Income objectuudiig uusgene.

--- app.js
+++ app.js
@@ -1,12 +1,55 @@
 //Дэлгэцтэй ажиллах контроллер
 var uiController = (function(){
-    
+    var DOMstrings = {
+        intputType: 'add__type',
+        inputDescription: '.add__description', 
+        inputValue: '.add__value',
+        addBtn: '.add__btn'
+    };
+    return {
+        getInput: function(){
+            return {
+                type: document.querySelector(DOMstrings.intputType).value, 
+                description: document.querySelector(DOMstrings.inputDescription).value, 
+                value: document.querySelector(DOMstrings.inputValue).value
+            }
+        },
+        getDOMstrings: function(){
+            return DOMstrings;
+        }
+    };
 })();
+
+
 
 //Санхүүтэй ажиллах контроллер
 var financeController = (function(){
-    
+
+    var Income = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+      };
+      
+      var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+      };
+
+      var data = {
+        allItems: {
+            inc: [],
+            exp: []
+        },
+        totals: {
+            inc: 0,
+            exp: 0
+        }
+      }  
 })();
+
+
 
 //Програмын холбогч контроллер
 var appController = (function(uiController, financeController){
@@ -21,16 +64,27 @@ var appController = (function(uiController, financeController){
         //4.Төсвийг тооцоолно.
 
         //5.Эцсийн үлдэгдэл тооцоог дэлгэцэнд гаргана.
-    }
+    };
    
-    document.querySelector('.add__btn').addEventListener('click', function(){
-        ctrlAddItem();
-    });
+    var setupEventListeners = function(){
+        var DOM = uiController.getDOMstrings();
 
-    document.addEventListener('keypress', function(event){ //event-tovchnii daragdsan elementtei holbootoi buh uildel
-        if (event.keyCode === 13 || event.which === 13){
+        document.querySelector(DOM.addBtn).addEventListener('click', function(){
             ctrlAddItem();
+        }); 
+        document.addEventListener('keypress', function(event){ //event-tovchnii daragdsan elementtei holbootoi buh uildel
+            if (event.keyCode === 13 || event.which === 13){
+                ctrlAddItem();
+            }
+        });
+    };
+
+    return {
+        init: function(){
+            console.log('app started...');
+            setupEventListeners();
         }
-    });
+    };
     
 })(uiController, financeController);
+appController.init();
