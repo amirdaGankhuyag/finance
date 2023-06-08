13a204184e24a9db982d8ed199b8b7d03b45b4d5
amirda <amirda.ganbold@gmail.com>
2023-06-08 18:40:14 +0800

    Finance module ruu item add hiideg bolgono

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var uiController = (function(){
     return {
         getInput: function(){
             return {
-                type: document.querySelector(DOMstrings.intputType).value, 
+                type: document.querySelector(DOMstrings.intputType).value, //exp, inc
                 description: document.querySelector(DOMstrings.inputDescription).value, 
                 value: document.querySelector(DOMstrings.inputValue).value
             }
@@ -24,21 +24,21 @@ var uiController = (function(){
 
 //Санхүүтэй ажиллах контроллер
 var financeController = (function(){
-
+    //private
     var Income = function(id, description, value) {
         this.id = id;
         this.description = description;
         this.value = value;
       };
-      
+      //private
       var Expense = function(id, description, value) {
         this.id = id;
         this.description = description;
         this.value = value;
       };
-
+      //private
       var data = {
-        allItems: {
+        items: {
             inc: [],
             exp: []
         },
@@ -46,7 +46,26 @@ var financeController = (function(){
             inc: 0,
             exp: 0
         }
-      }  
+      };
+      
+      return {
+        addItem: function(type, desc, val){
+            var item, id;
+
+            if(data.items[type].length === 0)   id = 1;
+            else {
+                id = data.items[type][data.items[type].length - 1].id + 1; 
+            }
+
+            if(type === 'inc'){
+                item = new Income(id, desc, val);
+            } else {
+                item = new Expense(id, desc, val);
+            }
+
+            data.items[type].push(item);
+        }
+      }
 })();
 
 
@@ -56,9 +75,9 @@ var appController = (function(uiController, financeController){
 
     var ctrlAddItem = function(){
         //1.Оруулах өгөгдлийг дэлгэцээс олж авна.
-
+        var input = uiController.getInput();
         //2.Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
-
+        financeController.addItem(input.type, input.description, input.value);
         //3.Олж авсан өгөгдлүүдээ веб дээрээ тохирох хэсэгт нь гаргана.
 
         //4.Төсвийг тооцоолно.
