4fbddb7cced5e6951dc19f7bb539d01a98a29442
amirda <amirda.ganbold@gmail.com>
2023-06-09 16:02:06 +0800

    tusviig delgetsend update hiine

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ var uiController = (function(){
         inputValue: '.add__value',
         addBtn: '.add__btn',
         incomeList: '.income__list',
-        expenseList: '.expenses__list'
+        expenseList: '.expenses__list',
+        tusuvLabel: '.budget__value',
+        incomeLebel: '.budget__income--value',
+        expenseLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage'
     };
     return {
         getInput: function(){
@@ -33,6 +37,17 @@ var uiController = (function(){
             //     fieldsArr[i].value = '';
             // }
         },
+        
+        tusviigUzuuleh: function(tusuv){
+            document.querySelector(DOMstrings.tusuvLabel).textContent = tusuv.tusuv;
+            document.querySelector(DOMstrings.incomeLebel).textContent = tusuv.totalInc;
+            document.querySelector(DOMstrings.expenseLabel).textContent = tusuv.totalExp;
+            if(tusuv.huvi !== 0){document.querySelector(DOMstrings.percentageLabel).textContent = tusuv.huvi + '%';
+            } else {
+                document.querySelector(DOMstrings.percentageLabel).textContent = tusuv.huvi;
+            }   
+        },
+
         addListItem: function(item, type){
             //1.Орлого зарлагын элементийг агуулсан HTML-ийг бэлтгэнэ.
             var html, list;
@@ -154,7 +169,7 @@ var appController = (function(uiController, financeController){
             //5.Эцсийн үлдэгдэл 
             var tusuv = financeController.tusviigAvah();
             //6.тооцоог дэлгэцэнд гаргана.
-            console.log(tusuv);
+            uiController.tusviigUzuuleh(tusuv);
         }  
     };
    
@@ -174,6 +189,12 @@ var appController = (function(uiController, financeController){
     return {
         init: function(){
             console.log('app started...');
+            uiController.tusviigUzuuleh({
+                tusuv: 0,
+                huvi: 0,
+                totalInc: 0,
+                totalExp: 0
+            });
             setupEventListeners();
         }
     };
